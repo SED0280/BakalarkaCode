0d61d6b0778674b1c036ce35e63f211ad4969195
SED0280 <michal.sedlar.st@vsb.cz>
2025-01-05 16:04:50 +0100

    NormalLegendre with 3-time recurtion

--- Kvadratury.py
+++ Kvadratury.py
@@ -1,5 +1,6 @@
 import numpy as np
 import scipy.integrate as integ
+import math
 
 def w(xzeros, j):
     prod = np.polynomial.Polynomial.fromroots([xzeros[i] for i in range(len(xzeros)) if i!=j])
@@ -10,13 +11,64 @@ def w(xzeros, j):
 def getWeights(xzeros):
     return [w(xzeros, i) for i in range(len(xzeros))]
 
-def generateLegendrePolynomials(x, n):
+def generateLegendre(x, n):
     alphak = 0
     ret = [np.zeros(len(x)), np.ones(len(x))]
     for k in range(2,n+1):
-        betak = 1/(4-k**(-2))
+        betak = 1/(4-(k-1)**(-2))
         pi = (x-alphak)*ret[-1]-betak*ret[-2]
         ret.append(pi)
     return np.array(ret[1:])
 
+def generateLegendrePoly(n):
+    alphak = 0
+    ret = [lambda x: 0, lambda x: 1]
+    for k in range(2,n+1):
+        betak = 1/(4-(k-1)**(-2)) # nebo mam pocitat pomoci integralu/G. kvadratur?
+        pi = (lambda x:(x-alphak)*ret[-1](x)-betak*ret[-2](x))
+        ret.append(pi)
+    return np.array(ret[1:])
 
+#print(generateLegendrePoly(2)[1](5))
+def generateLegendrePoly2(n):
+    ret = [lambda x: 0, lambda x: 1]
+    for k in range(2,n+1):
+        betak = 1/(4-(k-1)**(-2))
+        def pi(x, Pk_minus_1=ret[-1], Pk_minus_2=ret[-2], betak = betak):
+            alphak = 0 
+            #print("   ", betak)
+            return (x - alphak) * Pk_minus_1(x) - betak * Pk_minus_2(x)
+        #print(pk(1),"\n-------------------\n")
+        ret.append(pi)
+    return np.array(ret[1:])
+
+def generateNormalLegendrePoly2(n):
+    ret = [lambda x: 0, lambda x: 1]
+    
+    for k in range(2,n+1):
+        betak_minus_one = 1/(4-(k-1)**(-2))
+        betak = 1/(4-k**(-2))
+        def pi(x, Pi_minus_1=ret[-1], Pi_minus_2=ret[-2], betak = betak, betak_minus_one = betak_minus_one):
+            alphak = 0 
+            #print("   ", betak)
+            return ((x - alphak) * Pi_minus_1(x) - math.sqrt(betak_minus_one) * Pi_minus_2(x))/math.sqrt(betak)
+        #print(pk(1),"\n-------------------\n")
+        #print(lastbetak,"   ",betak,"\n")
+        ret.append(pi)
+    return np.array(ret[1:])
+
+n=10
+beta=[2]
+alpha=[0]
+for k in range(2,n):
+    beta.append(1/(4-(k-1)**(-2)))
+    alpha.append(0)
+alpha.append(0)
+
+a = np.diag(alpha)+ np.diag(beta,-1)+ np.diag(beta,1)
+
+print(beta)
+
+aaaaaa = generateNormalLegendrePoly2(n)
+
+print(aaaaaa[5](1))

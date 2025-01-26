4a00117b25621870482416ba4540a20ce36e3d51
SED0280 <michal.sedlar.st@vsb.cz>
2025-01-26 16:15:54 +0100

    Polynomials over interval <a,b>

--- Kvadratury.py
+++ Kvadratury.py
@@ -21,7 +21,40 @@ def generateLegendre(x, n):
         ret.append(pi)
     return np.array(ret[1:])
 
+def generateNormalLegendre(x, n):
+    ret = [np.zeros(len(x)), np.ones(len(x))]
+    alphak = 0
+    for k in range(2,n+1):
+        betak_minus_one = 1/(4-(k-1)**(-2))
+        betak = 1/(4-(k-1)**(-2))
 
+        pi = ((x - alphak) * ret[-1] - math.sqrt(betak_minus_one) * ret[-2])/math.sqrt(betak)
+        ret.append(pi)
+    return np.array(ret[1:])
+
+def generateLegendreOverInterval(x, n, a, b):
+    alphak = 0
+    ret = [np.zeros(len(x)), np.ones(len(x))]
+    for k in range(2,n+1):
+        betak = 1/(4-(k-1)**(-2))
+        pi = ( (((a+b)/(a-b) + (2/(b-a))*x) -alphak)*ret[-1]-betak*ret[-2] )
+        #pi =math.sqrt(2/(b-a))*pi
+        ret.append(pi)
+    return np.array(ret[1:])
+
+def generateNormalLegendreOverInterval(x, n, a, b):
+    ret = [np.zeros(len(x)), np.ones(len(x))]
+    alphak = 0
+    for k in range(2,n+1):
+        betak_minus_one = 1/(4-(k-1)**(-2))
+        betak = 1/(4-(k-1)**(-2))
+
+        pi = ((((a+b)/(a-b) + 2/(b-a)*x) - alphak) * ret[-1] - math.sqrt(betak_minus_one) * ret[-2])/math.sqrt(betak)
+        #pi = math.sqrt(2/(b-a))*pi
+        ret.append(pi)
+    return np.array(ret[1:])
+
+"""
 def generateLegendrePoly(n):
     ret = [lambda x: 0, lambda x: 1]
     for k in range(2,n+1):
@@ -48,36 +81,32 @@ def generateNormalLegendrePoly(n):
         #print(lastbetak,"   ",betak,"\n")
         ret.append(pi)
     return np.array(ret[1:])
+"""
 
-n=10
-beta=[2]
-alpha=[0]
-for k in range(2,n):
-    beta.append(1/(4-(k-1)**(-2)))
-    alpha.append(0)
-alpha.append(0)
+def JacobiGaussQuadLegendre(f, n):
+    Beta0 = 2
+    main_diag = np.zeros(n)
+    off_diag = np.array([ math.sqrt(1/(4- k**(-2))) for k in range(1,n)])
+    
+    eigenvalues, eigenvectors = eigh_tridiagonal(main_diag, off_diag)
 
-n=1000
-main_diag = np.zeros(n)  # main diagonal elements
-off_diag = np.array([ (1/(4-k**(-2)))**(1/2) for k in range(1,n)])      # off-diagonal elements
-print(off_diag)
-# Calculate eigenvalues and eigenvectors
-eigenvalues, eigenvectors = eigh_tridiagonal(main_diag, off_diag)
+    #J_n = np.diag(main_diag)
+    #J_n[1:,:len(J_n)-1]+=off_diag
+    #J_n[:len(J_n)-1,1:]+=off_diag
+    #print(np.dot(J_n,eigenvectors[:,0]))
+    #print(np.dot(eigenvalues[0],eigenvectors[:,0]))
 
-print("Eigenvalues (roots):", eigenvalues)
-print("Eigenvectors:", eigenvectors)
-Beta0 = 2
+    lam = Beta0 * eigenvectors[0]
 
-lam = Beta0 * eigenvectors[0]
-print("Lambda:", lam)
-print("Nodes:", eigenvalues)
+    return sum(lam*f(eigenvalues))
+
+#n=100
+#f = lambda x: x**2
+
+#print("Integral of x^2 over <-1,1> = ", JacobiGaussQuadLegendre(f, n))
+
+#J_n = np.diag(main_diag, off_diag)
+#print(J_n)
 #print(np.dot(J_n,eigenvectors[2]))
 #print(np.dot(eigenvalues[2],eigenvectors[2]))
-
-f = lambda x: np.sin(x)
-print(lam*f(eigenvalues))
-
-
-I = sum(lam*f(eigenvalues))
-print("Integral of x^2 over <-1,1> = ", I)
 

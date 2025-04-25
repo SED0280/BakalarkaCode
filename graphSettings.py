3121111231456297f65c4490689b8073c3f31d18
SED0280 <michal.sedlar.st@vsb.cz>
2025-04-25 22:37:36 +0200

    better gauss time loglog

--- graphSettings.py
+++ graphSettings.py
@@ -1,7 +1,7 @@
 
 from matplotlib import pyplot as plt
 plt.rc('font', family='Times Roman', size=11)  # Rodina a velikost písma
-plt.rc('figure', figsize=(6, 3), dpi=100)  # Velikost a rozlišení obrázku
+plt.rc('figure', figsize=(6.5, 3), dpi=100)  # Velikost a rozlišení obrázku
 plt.rc('text', usetex=True)
 plt.rc('grid', color='gray', linestyle='--')
 plt.rc('axes', grid=True)
